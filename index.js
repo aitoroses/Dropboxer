39e5bcb22190de879116d71f92cb6f4152244f7e
Aitor Oses <aitor.oses@gmail.com>
2014-06-09 09:10:04 +0200

    Benchmark

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var koa = require("koa")
-	, Router = require('koa-router')
-	, mount = require('koa-mount')
+var koa = require('koa'),
+    Router = require('koa-router'),
+    mount = require('koa-mount');
 
 // Custom Dropbox library
 var Dropbox = require('./lib');
@@ -8,11 +8,69 @@ var Dropbox = require('./lib');
 // Iniciar Koa
 var app = koa();
 
+/* Logger */
+app.use(function *(next) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms + 'ms');
+});
+
 var API = new Router();
 
 // GET accountInfo
-API.get("/accountInfo", function *() {
-	this.body = yield Dropbox.accountInfo();
+API.get('/accountInfo', function *() {
+    this.body = yield Dropbox.accountInfo();
+});
+
+// GET Dir list
+API.get('/list', function *() {
+    this.body = yield Dropbox.readdir('/');
+});
+
+// GET File stat
+API.get('/stat', function *() {
+    this.body = yield Dropbox.stat('zrc_cs.jpg');
+});
+
+// GET File API
+API.get('/file', function *() {
+    var stat = yield Dropbox.stat('zrc_cs.jpg');
+    this.set('Content-Type', stat.mimeType);
+    this.set('Content-Disposition', 'attachment; filename=' + stat.name);
+    this.body = yield Dropbox.readFile('zrc_cs.jpg');
+});
+
+// GET Benchmark
+API.get('/benchmark', function *() {
+    var photos =
+        [
+            'Foto 05-04-14 22 04 48.jpg',
+            'Foto 05-04-14 22 06 44.jpg',
+            'Foto 11-05-14 21 48 39.jpg',
+            'Foto 17-01-14 20 20 57.jpg'
+        ];
+    photos = photos.concat(photos);
+    photos = photos.concat(photos);
+    photos = photos.concat(photos);
+
+    // // Iteration using a for
+    // for (var i = 0; i < photos.length; i++) {
+    //     var name = photos[i];
+    //     console.log(name, i);
+    //     yield Dropbox.readFile(name);
+    // }
+
+    // Iteration using a map
+    var result = yield photos.map(function(name) {
+        return Dropbox.readFile(name);
+    });
+
+    for (var i = 0; i < result.length; i++) {
+        console.log(photos[i], result[i].slice(0, 20))
+    }
+
+    this.body = 'Done. Look console';
 });
 
 app.use(mount('/dropbox', API.middleware()));
--- lib.js
+++ lib.js
@@ -1,29 +1,42 @@
-var thunkify = require("thunkify"),
-	Dropbox = require("dropbox");
 
-var token = "gTwwMn7WAaoAAAAAAAAR5GYZDabJaQQD0SEnmxmQsc8Civ2GMBc_sttOfqJygpnc";
+var thunkify = require('thunkify'),
+    Dropbox = require('dropbox');
+
+var token = 'gTwwMn7WAaoAAAAAAAAR5GYZDabJaQQD0SEnmxmQsc8Civ2GMBc_sttOfqJygpnc';
 var client = new Dropbox.Client({
     token: token
 });
 
 // Default Callback function
 function C(callback) {
-	return function(error, data) {
-		if (error) {
-			return callback(error);  // Something went wrong.
-		}
-		callback(null, data);
-	};
+    return function(error, data) {
+        if (error) {
+            return callback(error);  // Something went wrong.
+        }
+        callback(null, data);
+    };
 }
 
-// AccountInfo API thunk
 function accountInfo(callback) {
-	client.getAccountInfo(C(callback));
+    client.getAccountInfo(C(callback));
 }
+/** Give dropbox account info thunk */
 exports.accountInfo = thunkify(accountInfo);
 
-// API for read a directory
 function readdir(dir, callback) {
-	client.readdir(dir, C(callback));
+    client.readdir(dir, C(callback));
 }
+/** Read a file from file system thunk */
 exports.readdir = thunkify(readdir);
+
+function readFile(file, callback) {
+    client.readFile(file, {buffer: true}, C(callback));
+}
+/** Read a file from dropbox thunk */
+exports.readFile = thunkify(readFile);
+
+function stat(file, callback) {
+    client.stat(file, C(callback));
+}
+/** Read file file stats from dropbox thunk */
+exports.stat = thunkify(stat);
